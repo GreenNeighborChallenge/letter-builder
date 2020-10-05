3722b7720aff5f33c908a4163b38ccd586f7a816
Jenni Schubert <jenni.schubert@gmail.com>
2020-10-05 16:29:58 -0500

    layout tweaks

--- src/components/StateGrade/StateGrade.jsx
+++ src/components/StateGrade/StateGrade.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+// import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import mapStoreToProps from '../../redux/mapStoreToProps';
@@ -34,10 +34,8 @@ const StateGrade = (props) => {
 
 
     return (
-        <div className={classes.container}>
-            <Card className={classes.root}>
-                <CardContent>
-                    <Typography variant='h4' gutterBottom>Your State: Add Dropdown</Typography>
+        <div>
+            <div id='stateTitle'><Typography variant='h4' gutterBottom>Your State: Add Dropdown</Typography></div>
                    <div className='outline'>
                     <Typography className={classes.title}>
                       Your state's energy and climate policy, graded:
@@ -60,12 +58,12 @@ const StateGrade = (props) => {
                     <Typography variant="h5" component="h2"></Typography>
                     <Typography className={classes.pos} color="textSecondary"></Typography>
                     <Typography variant="body2" component="p"></Typography>
-                </CardContent>
+
           
                     <Button style={{display: 'inline', float: 'left', margin: 5}}>Previous</Button>
                     <Button style={{display: 'inline',float: 'right', margin: 5}}>Create Your Letter!</Button>
          
-            </Card>
+ 
         </div>
     );
 }
--- src/components/ZipCode/ZipCode.jsx
+++ src/components/ZipCode/ZipCode.jsx
@@ -33,42 +33,43 @@ const useStyles = makeStyles({
 
 
 
-const ZipCode = ({dispatch, store}) => {
+const ZipCode = ({ dispatch, store }) => {
     // const dispatch = useDispatch();
     const classes = useStyles();
 
     let [zip, changeZip] = useState('');
 
-    function sendZip(){
-        dispatch({type: 'SEND_ZIP', payload: zip})
+    function sendZip() {
+        dispatch({ type: 'SEND_ZIP', payload: zip })
         console.log(zip)
     }
 
     return (
         <>
-        <div className={classes.container}>
-            <Card className={classes.root}>
-                <CardContent>
-                    <div style={{textAlign: 'center'}}>
-                    <Typography color="textSecondary" id='zipTitle' style={{fontSize: 48, fontFamily:'leafy', color:'black'}} gutterBottom>
-                        BE THE CHANGE: State Policy Petition Maker
+            <div className={classes.container}>
+                <Card className={classes.root}>
+                    <CardContent>
+                        <div style={{ textAlign: 'center' }}>
+                            <Typography color="textSecondary" id='zipTitle' style={{ fontSize: 48, fontFamily: 'leafy', color: 'black' }} gutterBottom>
+                                BE THE CHANGE: State Policy Petition Maker
         </Typography></div>
-                    <Typography className={classes.pos} color="textSecondary" variant='body1' style={{display: 'inline'}}> 
-                        The petition maker will walk you through your state's existing energy policies, what they mean, who has influence over them, 
-                        and help you send a letter to them advocating for green policies. <span style={{display: 'inline', float: 'right'}}><InfoPopover /></span>
-        </Typography> 
-                    <div className='zipBox' >
-                        <Typography variant='h4'>Enter Your Zip Code</Typography>
-                        <Typography variant="h5" component="h2">to find your state's policies and write to your elected officials</Typography>
-                        <TextField label="zip code" variant="outlined" onChange={(event) => changeZip(event.target.value)}/>
-                        <br />
-                        <Button variant='contained' onClick={sendZip}>Go</Button>
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
-        {store.zip.long_name &&
-        <StateGrade />}
+                        <Typography className={classes.pos} color="textSecondary" variant='body1' style={{ display: 'inline' }}>
+                            The petition maker will walk you through your state's existing energy policies, what they mean, who has influence over them,
+                        and help you send a letter to them advocating for green policies. <span style={{ display: 'inline', float: 'right' }}><InfoPopover /></span>
+                        </Typography>
+                        <div className='zipBox' >
+                            <Typography variant='h4'>Enter Your Zip Code</Typography>
+                            <Typography variant="h5" component="h2">to find your state's policies and write to your elected officials</Typography>
+                            <TextField label="zip code" variant="outlined" onChange={(event) => changeZip(event.target.value)} />
+                            <br />
+                            <Button variant='contained' onClick={sendZip}>Go</Button>
+                        </div>
+                        {store.zip.long_name &&
+                            <StateGrade />}
+                    </CardContent>
+                </Card>
+            </div>
+
         </>
     );
 }
