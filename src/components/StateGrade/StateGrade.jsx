39a805fe9675d566949025bd3c665bef9b18d5d0
Jenni Schubert <jenni.schubert@gmail.com>
2020-10-07 14:56:35 -0500

    matui list, ternary for policy value

--- src/components/StateGrade/StateGrade.jsx
+++ src/components/StateGrade/StateGrade.jsx
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import mapStoreToProps from '../../redux/mapStoreToProps';
@@ -6,72 +6,79 @@ import { connect } from 'react-redux'
 import GradeExplainer from './GradeExplainer.jsx'
 import './StateGrade.css'
 import PolicyExplainer from './PolicyExplainer.jsx';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 
-class StateGrade extends Component{
+class StateGrade extends Component {
 
-  
+
 
     componentDidMount() {
-        this.props.dispatch({type: 'GET_STATE_POLICIES', payload: this.props.stateInfo})
-        this.props.dispatch({type: 'FETCH_POLICIES'})
+        this.props.dispatch({ type: 'GET_STATE_POLICIES', payload: this.props.stateInfo })
+        this.props.dispatch({ type: 'FETCH_POLICIES' })
     }
 
     //searches by policy ID for policy name
     getById = (arr, value) => {
-        for (let i=0; i < arr.length; i++) {
-          if (arr[i].id === value) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].id === value) {
                 return arr[i].policy
-              }
-          }
+            }
         }
+    }
 
 
     render() {
-    return (
-        <div style={{backgroundColor: 'white'}}>
-            
-            {this.props.store.statePolicies && this.props.store.policyLanguage &&
-            <>
-            <div id='stateTitle'><Typography variant='h4' gutterBottom>Your State: Add Dropdown</Typography></div>
-                   <div className='outline'>
-                    <Typography>
-                      Your state's energy and climate policy, graded:
+        return (
+            <div style={{ backgroundColor: 'white' }}>
+
+                {this.props.store.statePolicies && this.props.store.policyLanguage &&
+                    <>
+                        <div id='stateTitle'><Typography variant='h4' gutterBottom>Your State: Add Dropdown</Typography></div>
+                        <div className='outline'>
+                            <Typography>
+                                Your state's energy and climate policy, graded:
         </Typography>
-        </div>
-            <div className='outline' id='grade'>
-            <Typography variant='h1'>{this.props.store.statePolicies.policy_grade}</Typography>
-            <GradeExplainer />
+                        </div>
+                        <div className='outline' id='grade'>
+                            <Typography variant='h1'>{this.props.store.statePolicies.policy_grade}</Typography>
+                            <GradeExplainer />
+                        </div>
+                        <Typography>
+                            Your state's existing energy and climate policies:
+        </Typography>
+                        <List>
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 1)}:<ListItemIcon><PolicyExplainer type={'CAP'} /></ListItemIcon>{this.props.store.statePolicies.climate_plan ? this.props.store.statePolicies.climate_plan : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 2)}:<ListItemIcon><PolicyExplainer type={'RPS'} /></ListItemIcon> {this.props.store.statePolicies.portfolio_standard ? this.props.store.statePolicies.portfolio_standard : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 5)}:<ListItemIcon><PolicyExplainer type={'GPM'} /></ListItemIcon>{this.props.store.statePolicies.green_pricing ? this.props.store.statePolicies.green_pricing : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 3)}:<ListItemIcon><PolicyExplainer type={'PACE'} /></ListItemIcon>{this.props.store.statePolicies.pace ? this.props.store.statePolicies.pace : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 9)}:<ListItemIcon><PolicyExplainer type={'EES'} /></ListItemIcon>{this.props.store.statePolicies.energy_standard ? this.props.store.statePolicies.energy_standard : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 4)}:<ListItemIcon><PolicyExplainer type={'CVP'} /></ListItemIcon>{this.props.store.statePolicies.clean_vehicle ? this.props.store.statePolicies.clean_vehicle : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 7)}:<ListItemIcon><PolicyExplainer type={'CS'} /></ListItemIcon>{this.props.store.statePolicies.community_solar ? this.props.store.statePolicies.community_solar : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 6)}:<ListItemIcon><PolicyExplainer type={'HSR'} /></ListItemIcon>{this.props.store.statePolicies.home_solar ? this.props.store.statePolicies.home_solar : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 8)}:<ListItemIcon><PolicyExplainer type={'CCA'} /></ListItemIcon>{this.props.store.statePolicies.community_choice ? this.props.store.statePolicies.community_choice : <p>none</p>}</ListItem>
+
+                            <ListItem>{this.getById(this.props.store.policyLanguage, 10)}:<ListItemIcon><PolicyExplainer type={'CUB'} /></ListItemIcon>{this.props.store.statePolicies.utility_board ? this.props.store.statePolicies.utility_board : <p>none</p>}</ListItem>
+
+                        </List>
+
+                        <Button style={{ display: 'inline', float: 'left', margin: 5 }}>Previous</Button>
+                        <Button style={{ display: 'inline', float: 'right', margin: 5 }}>Create Your Letter!</Button>
+                    </>
+                }
             </div>
-            <Typography>
-                      Your state's existing energy and climate policies:
-        </Typography>
-            <ul>
-            <li>{this.getById(this.props.store.policyLanguage, 1)}
-            <PolicyExplainer type={'CAP'}/>: {this.props.store.statePolicies.climate_plan}</li>
-            <li>{this.getById(this.props.store.policyLanguage, 2)} 
-            <PolicyExplainer type={'RPS'}/>: <span>{this.props.store.statePolicies.portfolio_standard}</span></li>
-            <li>{this.getById(this.props.store.policyLanguage, 5)}
-            <PolicyExplainer type={'GPM'} />:<span>{this.props.store.statePolicies.green_pricing}</span></li>
-            <li>{this.getById(this.props.store.policyLanguage, 3)}
-            <PolicyExplainer type={'PACE'} />: <span>{this.props.store.statePolicies.pace}</span></li>
-        
-            <li>{this.getById(this.props.store.policyLanguage, 9)} 
-            <PolicyExplainer type={'EES'} />: <span>{this.props.store.statePolicies.energy_standard}</span></li>
-        
-            <li>{this.getById(this.props.store.policyLanguage, 4)}:</li>
-            <li>{this.getById(this.props.store.policyLanguage, 6)}:</li>
-            <li>{this.getById(this.props.store.policyLanguage, 7)}:</li>
-            <li>{this.getById(this.props.store.policyLanguage, 8)}:</li>
-            <li>{this.getById(this.props.store.policyLanguage, 10)}:</li>
-        </ul>
-          
-                    <Button style={{display: 'inline', float: 'left', margin: 5}}>Previous</Button>
-                    <Button style={{display: 'inline',float: 'right', margin: 5}}>Create Your Letter!</Button>
-                </>
-            }
-        </div>
-    );
+        );
     }
 }
 
