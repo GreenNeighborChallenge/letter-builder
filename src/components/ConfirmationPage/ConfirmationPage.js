ac758322d2646fcb750dfc1024e22c6884f51cff
Paul Buysse <paulbuysse@Pauls-MacBook-Pro.local>
2020-10-08 10:50:46 -0500

    created confirmation page

--- src/components/ConfirmationPage/ConfirmationPage.js
+++ src/components/ConfirmationPage/ConfirmationPage.js
@@ -13,16 +13,23 @@ class ConfirmationPage extends Component {
         return (
             <div className="confirmationBody">
                 <div className="confirmation">
-                    <h2>{this.state.heading}</h2>
-                    <h1>Success!</h1>
+                    <h1 style={{ fontSize: 48, fontFamily: 'leafy', color: 'black' }}>Success!</h1>
                     <h4>
                         Thank you for taking the time to contact your state officials.
                         You will recieve an email copy of your letter for your records.
                     </h4>
-                    <Link className="confirmationBtnShare leafy">Share</Link>
-                    <p></p>
-                    <Link to="https://www.greenneighborchallenge.com/" className="confirmationBtnHome leafy">Green Neighbor Home</Link>
-                    <p>Find more ways to get involved!</p>
+                    <div className="confirmBtns">
+                        <div className="confirmationBtnShare">
+                            <h3 style={{ fontSize: 25, fontFamily: 'leafy', color: 'black' }}>Share</h3>
+                            <h5>Challenge your friends!</h5>
+                        </div>
+                        <a className="unstyled" href="https://www.greenneighborchallenge.com/">
+                            <div className="confirmationBtnHome">
+                                <h3 style={{ fontSize: 25, fontFamily: 'leafy', color: 'black' }}>Green Neighbor Home</h3>
+                                <h5>Find more ways to get involved!</h5>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         );
