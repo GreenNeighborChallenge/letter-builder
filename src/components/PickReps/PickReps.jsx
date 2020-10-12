e06a1723a1de45b3c9f188f0728b0867846c54b0
Jenni Schubert <jenni.schubert@gmail.com>
2020-10-12 13:24:53 -0500

    preview letter fix

--- src/components/PickReps/PickReps.jsx
+++ src/components/PickReps/PickReps.jsx
@@ -109,10 +109,10 @@ const PickReps = ({ dispatch, reps, history, offices }) => {
                 <CardActions className={cardActions}>
                     
                 <div className={right}>
-                {/* <Button variant="outlined" onClick={directToPreview}>
+                <Button variant="outlined" onClick={directToPreview}>
                     Preview Letter!
-                </Button> */}
-                    <PreviewLetter selections={selections} />
+                </Button>
+                    {/* <PreviewLetter selections={selections} /> */}
                 </div>
                 <div className={left}>
                 <Button variant="outlined" onClick={directToAddressForm}> Back </Button>
