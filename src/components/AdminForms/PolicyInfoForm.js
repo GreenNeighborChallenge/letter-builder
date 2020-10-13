6a3fb73a5032683be8600d501791dd658da78921
rileyww77 <riley_ww77@yahoo.com>
2020-10-13 10:32:32 -0500

    successfully adding policy info

--- src/components/AdminForms/PolicyInfoForm.js
+++ src/components/AdminForms/PolicyInfoForm.js
@@ -7,51 +7,44 @@ import Button from '@material-ui/core/button'
 import TextField from '@material-ui/core/TextField';
 import './AdminForms.css';
 
-function PolicyInfoForm({dispatch, store, state_name}) {
+function PolicyInfoForm({ dispatch, store }) {
 
     const { handleSubmit, register, reset } = useForm();
 
     const onSubmit = (data) => {
-        const newData = {...data, state_name}
-        dispatch ({ type: 'SET_POLICY_INFO', payload: newData})
+        const newData = { ...data, id: store.adminState.id }
+        console.log(newData)
+        dispatch({ type: 'SET_POLICY_INFO', payload: newData })
         reset()
     }
 
     useEffect(() => {
         dispatch({ type: 'FETCH_POLICIES' });
     }, [dispatch])
-    
 
 
-        return (
-            <div>
-                <h1>State Policy Info</h1>
-                <FormControl>
+
+    return (
+        <div>
+            <h1>State Policy Info</h1>
+            <FormControl>
                 <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
-                {/* <p>State Grade:</p>
-                <TextField inputRef={register}  label="State Grade" variant="outlined" size="small" name="State Grade" placeholder="State Grade" />
-                <br /> */}
-                {store.policyLanguage.map((policy) => {
-                    return(
-                        <>
-                        <p>{policy.name}:</p>
-                       <TextField inputRef={register}  label={policy.name} variant="outlined" size="small" name={policy.id} placeholder={policy.name} />
-                       <br />
-                       </>
-                    )
-                })}
-                 {/* <br />
-                 <TextField inputRef={register}  label="Resident Count" variant="outlined" size="small" name="Resident Count" placeholder="Resident Count"/>
-                 <br />
-                 <TextField inputRef={register}  label="Resident MWH" variant="outlined" size="small" name="Resident MWH" placeholder="Resident MWH" />
-                <br /> */}
-                <Button type="submit">save</Button>
-                    </form>
-                </FormControl>
-               
-            </div>
-        );
-    }
+                    {store.policyLanguage.map((policy) => {
+                        return (
+                            <>
+                                <p>{policy.name}:</p>
+                                <TextField inputRef={register} label={policy.name} variant="outlined" size="small" name={policy.id} placeholder={policy.name} />
+                                <br />
+                            </>
+                        )
+                    })}
+                    <Button type="submit">save</Button>
+                </form>
+            </FormControl>
+
+        </div>
+    );
+}
 
 
 export default connect(mapStoreToProps)(PolicyInfoForm);
--- src/redux/sagas/AdminForm.saga.js
+++ src/redux/sagas/AdminForm.saga.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
+// import { connect } from 'react-redux';
+// import mapStoreToProps from '../mapStoreToProps.js'
 
 
 //saga for state contact info
@@ -24,22 +26,22 @@ function* setNewSseo(action){
 
 //this is to get the state id so we are able to 
 //save the correct info in the database
-function* getStateId(action) {
-    try {
-        console.log(action.payload)
-        let response = yield axios.get(`api/admin/${action.payload.state_name}`);
-        console.log(response.data)
-        yield put({ type: 'SET_STATE_ID', payload: response.data })
-    } catch (error) {
-        console.log('error getting state id', error)
-    }
-}
+// function* getStateId(action) {
+//     try {
+//         console.log(action.payload)
+//         let response = yield axios.get(`api/admin/${action.payload.state_name}`);
+//         console.log(response.data)
+//         yield put({ type: 'SET_STATE_ID', payload: response.data })
+//     } catch (error) {
+//         console.log('error getting state id', error)
+//     }
+// }
 
-//sagas for the policy info
+//set new policy info
 function* setPolicyInfo(action) {
     try {
-        console.log(action.payload)
-        let response = yield axios.post(`/api/admin/policy`, action.payload);
+        //put id in URL so we can still use it after it's deleted from the object (done on server)
+        let response = yield axios.post(`/api/admin/policy/${action.payload.id}`, action.payload);
         console.log(response.data)
     } catch (error) {
         console.log('error setting policy info', error)
@@ -95,7 +97,7 @@ function* AdminFormSaga() {
     yield takeLatest('SET_CONTACT_INFO', setContactInfo);
     yield takeLatest('SET_POLICY_INFO', setPolicyInfo);
     yield takeLatest('SET_NEW_SSEO', setNewSseo)
-    yield takeLatest('GET_STATE_ID', getStateId);
+    // yield takeLatest('GET_STATE_ID', getStateId);
     yield takeLatest('NEW_POLICY_LANGUAGE', newPolicyLanguage);
     yield takeLatest('UPDATE_POLICY_LANGUAGE', updatePolicyLanguage);
     yield takeLatest('DELETE_POLICY', deletePolicy);
