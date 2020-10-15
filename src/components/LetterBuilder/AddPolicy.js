870c78975cef5eda6a133df325243cc7e3c79eb8
jpmzurk <jpmzurk@gmail.com>
2020-10-15 09:02:50 -0500

    added filter to change policy button to delete if text already exists

--- src/components/LetterBuilder/AddPolicy.js
+++ src/components/LetterBuilder/AddPolicy.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+// import mapStoreToProps from '../../redux/mapStoreToProps';
 import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -10,22 +10,30 @@ const useStyles = makeStyles({
     }
 });
 
-function AddPolicy({ policy, zip, dispatch}) {
+function AddPolicy({ policy, zip, dispatch, letter }) {
     const { root } = useStyles();
+
     //adding policy to letter
     const [addPolicy, setAddPolicy] = useState(false)
-    
+
+    //check if ids are already in bodyIds array
+    useEffect(() => {
+        const result = letter.bodyIds.some(id => policy.id === id)
+        if (result === true) {
+            setAddPolicy(true)
+        }
+    }, []);
+
     const handleAdd = () => {
-        const data = {state: zip.long_name, id: policy.id }
+        const data = { state: zip.long_name, id: policy.id }
         dispatch({ type: 'POLICY_TO_LETTER', payload: data });
         setAddPolicy(true)
     }
 
-    const handleDelete = (e) => {        
+    const handleDelete = (e) => {
         setAddPolicy(false)
         const filteredPolicy = policy.petition_info.replaceAll("[STATE]", zip.long_name)
-
-        dispatch({ type: 'DELETE_POLICY_FROM_LETTER', payload: {text: filteredPolicy, id: policy.id}})
+        dispatch({ type: 'DELETE_POLICY_FROM_LETTER', payload: { text: filteredPolicy, id: policy.id } })
     }
 
     return (
@@ -34,9 +42,13 @@ function AddPolicy({ policy, zip, dispatch}) {
                 <Button color="primary" onClick={handleDelete}>Delete</Button> :
                 <Button color="primary" onClick={handleAdd}>Add</Button>
             }
-         
+
         </div>
     );
 }
-
+const mapStoreToProps = (reduxState) => {
+    return {
+        letter: reduxState.letter,
+    };
+};
 export default connect(mapStoreToProps)(AddPolicy);
