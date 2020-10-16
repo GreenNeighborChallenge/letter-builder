cc0e7a74ef81a98ca573171322c2a59167de654f
Jenni Schubert <jenni.schubert@gmail.com>
2020-10-16 12:07:48 -0500

    map & split long text

--- src/components/PolicyExplainer/PolicyExplainer.jsx
+++ src/components/PolicyExplainer/PolicyExplainer.jsx
@@ -10,25 +10,25 @@ import { connect } from 'react-redux'
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import Tooltip from '@material-ui/core/Tooltip';
 import {
-    createMuiTheme,
-    MuiThemeProvider,
+  createMuiTheme,
+  MuiThemeProvider,
 } from "@material-ui/core/styles";
 
 
 //override button styling?
 // const defaultTheme = createMuiTheme();
 const theme = createMuiTheme({
-    overrides: {
-        MuiTooltip: {
-            tooltip: {
-                fontSize: "1em",
-                color: "black",
-                backgroundColor: "white",
-                boxShadow: "0 2px 2px 2px rgba(0, 0, 0, .4)",
-                fontWeight: 'normal',
-            }
-        }
+  overrides: {
+    MuiTooltip: {
+      tooltip: {
+        fontSize: "1em",
+        color: "black",
+        backgroundColor: "white",
+        boxShadow: "0 2px 2px 2px rgba(0, 0, 0, .4)",
+        fontWeight: 'normal',
+      }
     }
+  }
 });
 
 function PolicyExplainer(props) {
@@ -43,32 +43,40 @@ function PolicyExplainer(props) {
   };
 
   return (
-    <div style={{display: 'inline'}}>
+    <div style={{ display: 'inline' }}>
       <MuiThemeProvider theme={theme}>
-      <Tooltip  title={props.toolTitle}>
-      <Button color="primary" onClick={handleClickOpen} style={{color: 'black'}} id={props.id}>
-        {props.policy_name}
-        <HelpOutlineIcon />
-      </Button>
-      </Tooltip>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {props.text}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Close
+        <Tooltip title={props.toolTitle}>
+          <Button color="primary" onClick={handleClickOpen} style={{ color: 'black' }} id={props.id}>
+            {props.policy_name}
+            <HelpOutlineIcon />
           </Button>
-        </DialogActions>
-      </Dialog>
+        </Tooltip>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {/* {props.text} */}
+              {props.text.split('\n').map(function (item, key) {
+                return (
+                  <span key={key}>
+                    {item}
+                    <br />
+                  </span>
+                )
+              })}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary" autoFocus>
+              Close
+          </Button>
+          </DialogActions>
+        </Dialog>
       </MuiThemeProvider>
     </div>
   );
